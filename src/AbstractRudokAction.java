b2dd2f0ad6e22df83f0b317dde0f1fa4a09793e3
Dusan Tisma <tismadusan567@gmail.com>
2021-10-24 00:53:06 +0200

    added AbstractRudokAction

---
+++ src/AbstractRudokAction.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.net.URL;
+
+public abstract class AbstractRudokAction extends AbstractAction {
+
+    public Icon loadIcon(String filename) {
+        URL imageURL = getClass().getResource(filename);
+        Icon icon = null;
+
+        if(imageURL != null) {
+            icon = new ImageIcon(imageURL);
+        } else {
+            System.err.println("Resource not found" + filename);
+        }
+        return icon;
+    }
+}
