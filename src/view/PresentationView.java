50b1614aed5f4e6381b0535f408495743aec2d64
Dusan Tisma <tismadusan567@gmail.com>
2021-11-04 17:58:30 +0100

    Slides scroll pane in presentationview

--- src/view/PresentationView.java
+++ src/view/PresentationView.java
@@ -1,9 +1,12 @@
 package view;
 
 import model.Presentation;
+import model.RuNode;
+import model.Slide;
 
 import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
 
 public class PresentationView extends JPanel {
     private Presentation presentation;
@@ -13,14 +16,37 @@ public class PresentationView extends JPanel {
 
     public PresentationView(Presentation presentation) {
         setLayout(new BorderLayout(0, 30));
+
+//        author label
         lblAuthor = new JLabel(presentation.getAuthor(), SwingConstants.CENTER);
         lblAuthor.setFont(new Font("Dialog", Font.BOLD, 20));
         add(lblAuthor, BorderLayout.NORTH);
 
 //        Image image = new ImageIcon(presentation.getImagePath()).getImage();
-        Image image = new ImageIcon(getClass().getResource(presentation.getImagePath())).getImage(); //todo: check null
-        slideView = new SlideView(presentation.getSlideAt(slideIndex), image);
-        add(slideView, BorderLayout.CENTER);
+        URL imageURL = getClass().getResource(presentation.getImagePath());
+        Image image = null;
+        if(imageURL != null) {
+            image = new ImageIcon(imageURL).getImage();
+        } else {
+            System.err.println("Image not found"); //todo: handle
+        }
+        JPanel panel = new JPanel();
+        BoxLayout boxLayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
+        panel.setLayout(boxLayout);
+        for(int i=0;i<10;i++) {
+            panel.add(Box.createVerticalStrut(50));
+            panel.add(new SlideView(presentation.getSlideAt(slideIndex), image));
+        }
+        for(RuNode child : presentation.getChildren()) {
+            panel.add(Box.createVerticalStrut(50));
+            panel.add(new SlideView((Slide) child, image));
+        }
+
+        JScrollPane slidesScrollPane = new JScrollPane(panel);
+        slidesScrollPane.getVerticalScrollBar().setUnitIncrement(16);
+
+//        slidesScrollPane.add(slideView);
+        add(slidesScrollPane, BorderLayout.CENTER);
 
     }
 }
--- src/view/SlideView.java
+++ src/view/SlideView.java
@@ -3,25 +3,33 @@ package view;
 import model.Slide;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class SlideView extends JPanel {
     private Slide slide;
     private Image image;
+    private Dimension dimension;
 
     public SlideView(Slide slide, Image image) {
         this.slide = slide;
-        this.image = image;
+        this.dimension = new Dimension(1066, 600);
+        this.image = image.getScaledInstance(dimension.width, dimension.height, Image.SCALE_SMOOTH);
         init();
     }
 
     private void init() {
+        setPreferredSize(dimension);
+        setMaximumSize(dimension);
+        setAlignmentX(CENTER_ALIGNMENT);
+
         add(new JLabel("ASD"));
+
         setVisible(true);
     }
 
     public void displaySlide(Slide slide) {
-        this.slide = slide;
         //todo
     }
 
@@ -29,5 +37,6 @@ public class SlideView extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+//        g.drawImage(image, 0, 0, this);
     }
 }
